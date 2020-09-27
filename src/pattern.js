50109e1400c7be300ddb636f8786b6e4fe9847aa
Rob Waller <rdwaller1984@gmail.com>
2020-09-27 19:11:42 +0100

    Export pattern method from pattern module and integrate with cells module.

--- src/pattern.js
+++ src/pattern.js
@@ -1,30 +1,28 @@
-const pattern = () => {
-  const printPattern = (state) => {
-    let cellPatternCollection = document.createElement('div');
-    cellPatternCollection.className = 'collection';
-    
-    state.forEach(cell => {
-      let patternCell = document.createElement('div');
-      if (cell === 0) {
-        patternCell.className = 'dead';
-      } else {
-        patternCell.className = 'alive';
-      }
-      cellPatternCollection.append(patternCell);
-    });
-    
-    cellsPattern = document.getElementById('cells-pattern');
-    cellsPattern.append(cellPatternCollection);
-  }
-  
-  let patternState = initialState;
-  
-  console.log(patternState);
+import { iterateCells } from './cells.js';
 
-  printPattern(patternState);
-  
+const printPattern = (state) => {
+  let cells = document.createElement('div');
+  cells.className = 'collection';
+    
+  state.forEach(cell => {
+    let patternCell = document.createElement('div');
+    if (cell === 0) {
+      patternCell.className = 'dead';
+    } else {
+      patternCell.className = 'alive';
+    }
+    cells.append(patternCell);
+  });
+    
+  let cellsPattern = document.getElementById('cells-pattern');
+  cellsPattern.append(cells);
+}
+
+export const pattern = (state) => {
+  printPattern(state);
+    
   for (let i = 0; i < 198; i++) {
-    patternState = iterateCells(patternState);
-    printPattern(patternState);
+    state = iterateCells(state);
+    printPattern(state);
   }
 }
